78eed4139deae3e88c15d73eb2e53887256cb445
karmide <81155177+ChiaraParente@users.noreply.github.com>
2022-02-03 15:49:50 +0100

    FINITI NPC + aggiunto asset FX

--- Assets/Scripts/NPC_controller.cs
+++ Assets/Scripts/NPC_controller.cs
@@ -5,18 +5,23 @@ using UnityEngine.AI;
 
 public class NPC_controller : MonoBehaviour
 {
-    private NavMeshAgent myAgent;
+    public NavMeshAgent myAgent;
     private GameObject[] availablePlaces;
     public Player_Controller player;
     private Animator animator;
     private int lastShelfChosen;
     private Ray ray;
     private RaycastHit hit;
+    private Ray rayCam;
+    private RaycastHit hitCam;
     private bool alreadyWaiting = false;
-    private int seed = 1900;
-    private float smooth = 5.0f;
+    private int seed = 4679;
+    private float smooth = 0.3f;
     private bool isWalking;
     public float animationSpeed;
+    public int animationCounter = 1;
+    private string triggerName;
+    public bool isAnimatedClick = false;
 
     private void Awake()
     {
@@ -38,31 +43,53 @@ public class NPC_controller : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (!player.inventario)
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(animationCounter.ToString()) || animator.GetCurrentAnimatorStateInfo(0).IsName(animationCounter.ToString() + " 0") || animator.GetCurrentAnimatorStateInfo(0).IsName(animationCounter.ToString() + " 1"))
         {
-            if (myAgent.isStopped)
-            {
-                myAgent.isStopped = false;
-                if (!isWalking) animator.speed = 1;
-            }
-            ray = new Ray(transform.position, Vector3.forward);
-            if (DestinationReached() && !alreadyWaiting)
-            {
-                StartCoroutine(Waiter());
-            }
-            else if (Physics.Raycast(ray, out hit, 0.9f))
-            {
-                if (hit.transform.gameObject.tag == "NPC" && hit.transform != transform && hit.normal.magnitude - myAgent.transform.forward.magnitude < 0.1 && hit.normal.magnitude - myAgent.transform.forward.magnitude > -0.1)
-                {
-                    SetDestination();
-                }
-            }
-            if (isWalking) animator.speed = myAgent.velocity.magnitude * animationSpeed;
+            myAgent.velocity = Vector3.zero;
+            animator.speed = 1f;
         }
         else
         {
-            animator.speed = 0f;
-            myAgent.isStopped = true;
+            if (!player.inventario)
+            {
+                if (myAgent.isStopped)
+                {
+                    myAgent.isStopped = false;
+                    if (!isWalking) animator.speed = 1;
+                }
+                ray = new Ray(transform.position, Vector3.forward);
+                if (DestinationReached() && !alreadyWaiting)
+                {
+                    StartCoroutine(Waiter());
+                }
+                else if (Physics.Raycast(ray, out hit, 0.9f))
+                {
+                    if (hit.transform.gameObject.tag == "NPC" && hit.transform != transform && hit.normal.magnitude - myAgent.transform.forward.magnitude < 0.1 && hit.normal.magnitude - myAgent.transform.forward.magnitude > -0.1)
+                    {
+                        SetDestination();
+                    }
+                }
+                if (isWalking) animator.speed = myAgent.velocity.magnitude * animationSpeed;
+
+
+                rayCam = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(rayCam, out hitCam, 10.0f))
+                {
+                    if (Input.GetMouseButtonDown(0) && hitCam.collider.Equals( myAgent.gameObject.GetComponent<Collider>() ) )
+                    {
+                        isAnimatedClick = true;
+                        StartCoroutine(RotateMeToPlayer( Quaternion.LookRotation(player.transform.position - transform.position, Vector3.up )));
+                        triggerName = "Trigger" + animationCounter;
+                        
+                        animator.SetTrigger(triggerName);
+                    }
+                }
+            }
+            else
+            {
+                animator.speed = 0f;
+                myAgent.isStopped = true;
+            }
         }
     }
 
@@ -73,7 +100,7 @@ public class NPC_controller : MonoBehaviour
 
         animator.speed = 1;
         isWalking = false;
-        StartCoroutine(RotateMe());
+        StartCoroutine(RotateMeToShelf(availablePlaces[lastShelfChosen].transform.rotation));
         yield return new WaitForSecondsRealtime(Random.Range(3, 7));
         animator.SetBool("isWalking", true);
         isWalking = true;
@@ -81,20 +108,41 @@ public class NPC_controller : MonoBehaviour
         alreadyWaiting = false;
     }
 
-    IEnumerator RotateMe()
+    IEnumerator RotateMeToShelf( Quaternion rot)
     {
-        while (transform.rotation != availablePlaces[lastShelfChosen].transform.rotation)
+        float slider = 0;
+        while (slider < 1) 
         {
+            slider += Time.deltaTime * smooth;
+
             if (!alreadyWaiting)
             {
                 yield break;
             }
-            transform.rotation = Quaternion.Slerp(transform.rotation, availablePlaces[lastShelfChosen].transform.rotation, Time.deltaTime * smooth);
-            yield return null;
+            if(! isAnimatedClick) transform.rotation = Quaternion.Slerp(transform.rotation, rot, slider);
+            yield return new WaitForEndOfFrame();
         }
+        yield return null;
     }
 
-    private void SetDestination()
+    IEnumerator RotateMeToPlayer(Quaternion rot)
+    {
+        float slider = 0;
+        while (slider < 1)
+        {
+            slider += Time.deltaTime * smooth;
+
+            if (!isAnimatedClick)
+            {
+                yield break;
+            }
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, slider);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+
+    void SetDestination()
     {
         NavMeshPath path = new NavMeshPath();
         Vector3 randomPosition = FindRandomPosition();
