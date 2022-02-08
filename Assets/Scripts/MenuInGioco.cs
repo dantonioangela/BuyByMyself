9fcca5f85ef20189f4f40d7b5c5e9209e26ce15d
GiacomoOffre <giacomo.offre@gmail.com>
2022-02-08 22:15:37 +0100

    Aggiunta opzione nei menu

--- Assets/Scripts/MenuInGioco.cs
+++ Assets/Scripts/MenuInGioco.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuInGioco : MonoBehaviour
 {
@@ -15,6 +16,29 @@ public class MenuInGioco : MonoBehaviour
     public GameObject MainMenuUI;
 
     public AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+    public Dropdown ResolutionDropdownUI;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        ResolutionDropdownUI.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i=0; i<resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        ResolutionDropdownUI.AddOptions(options);
+        ResolutionDropdownUI.value = currentResolutionIndex;
+        ResolutionDropdownUI.RefreshShownValue();
+    }
 
     // Update is called once per frame
     void Update()
@@ -75,6 +99,22 @@ public class MenuInGioco : MonoBehaviour
     public void Volume(float volume)
     {
         audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetGrafica(int graficaIndex)
+    {
+        QualitySettings.SetQualityLevel(graficaIndex);
+    }
+
+    public void SchermoIntero(bool isSchermoIntero)
+    {
+        Screen.fullScreen = isSchermoIntero;
+    }
+
+    public void SetRisoluzione(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
 }
--- Assets/Scripts/MenuPrincipale.cs
+++ Assets/Scripts/MenuPrincipale.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuPrincipale : MonoBehaviour
 {
@@ -13,12 +14,32 @@ public class MenuPrincipale : MonoBehaviour
 
     static public bool MainMenuActive = false;
 
+    Resolution[] resolutions;
+    public Dropdown MAINResolutionDropdownUI;
+
     // Start is called before the first frame update
     void Start()
     {
         player.UI_active = true;
         MainMenuUI.SetActive(true);
         MainMenuActive = true;
+
+        resolutions = Screen.resolutions;
+        MAINResolutionDropdownUI.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        MAINResolutionDropdownUI.AddOptions(options);
+        MAINResolutionDropdownUI.value = currentResolutionIndex;
+        MAINResolutionDropdownUI.RefreshShownValue();
     }
 
     public void StartGame()
@@ -59,5 +80,11 @@ public class MenuPrincipale : MonoBehaviour
     public void Tutorial()
     {
         Debug.Log("Tutorial...");
+    }
+
+    public void SetRisoluzione(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
