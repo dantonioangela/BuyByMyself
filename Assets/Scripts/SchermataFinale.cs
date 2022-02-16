c4652bfd34dd00331eb8834a9b9832e8ac466177
GiacomoOffre <giacomo.offre@gmail.com>
2022-02-16 20:37:03 +0100

    schermate di fine gioco

--- Assets/Scripts/SchermataFinale.cs
+++ Assets/Scripts/SchermataFinale.cs
@@ -7,14 +7,33 @@ using UnityEngine.SceneManagement;
 public class SchermataFinale : MonoBehaviour
 {
     public float Massimo=100;
-    public Image MaskTotale;
-    public Image MaskPackaging;
-    public Image MaskQuality;
-    public Image MaskStagione;
-    public Image MaskProvenienza;
-    public Image MaskPrezzo;
+
+    public Image MaskTotaleFacile;
+    public Image MaskTotaleMedia;
+    public Image MaskTotaleDifficile;
+
+    public Image MaskPackagingFacile;
+    public Image MaskPackagingMedia;
+    public Image MaskPackagingDifficile;
+
+    public Image MaskQualityFacile;
+    public Image MaskQualityMedia;
+    public Image MaskQualityDifficile;
+
+    public Image MaskStagioneDifficile;
+
+    public Image MaskProvenienzaMedia;
+    public Image MaskProvenienzaDifficile;
+
+    public Image MaskPrezzoFacile;
+    public Image MaskPrezzoMedia;
+    public Image MaskPrezzoDifficile;
 
     private Result result;
+
+    public GameObject SchermataFacile;
+    public GameObject SchermataMedia;
+    public GameObject SchermataDifficile;
 
     // Start is called before the first frame update
     void Start()
@@ -32,28 +51,99 @@ public class SchermataFinale : MonoBehaviour
     {
         result = FinalResultCalculator.calculateFinalResult(Carrello_controller.prodottiNelCarrello, MenuPrincipale.levelDifficulty);
 
-        float fillAmountTotale = (float)result.totalPoints/ (float)Massimo;
-        MaskTotale.fillAmount = fillAmountTotale;
+        float fillAmountTotale = 0;
+        float fillAmountPrezzo = 0;
+        float fillAmountPackaging = 0;
+        float fillAmountQuality = 0;
+        float fillAmountProvenienza = 0;
+        float fillAmountStagione = 0;
 
-        float fillAmountPackaging = (float)result.ecoPoints / (float)Massimo;
-        MaskPackaging.fillAmount = fillAmountPackaging;
-
-        float fillAmountQuality = (float)result.qualityPoints/ (float)Massimo;
-        MaskQuality.fillAmount = fillAmountQuality;
-
-        float fillAmountStagione = (float)result.seasonPoints/ (float)Massimo;
-        MaskStagione.fillAmount = fillAmountStagione;
-
-        float fillAmountProvenienza = (float)result.originPoints / (float)Massimo;
-        MaskProvenienza.fillAmount = fillAmountProvenienza;
-
-        float fillAmountPrezzo = (float)result.pricePoints / (float)Massimo;
-        MaskPrezzo.fillAmount = fillAmountPrezzo;
+        if (MenuPrincipale.levelDifficulty == 0)
+        {
+            fillAmountTotale = (float)result.totalPoints / (float)Massimo;
+            fillAmountPrezzo = (float)result.pricePoints / (float)Massimo;
+            MaskTotaleFacile.fillAmount = fillAmountTotale;
+            MaskPrezzoFacile.fillAmount = fillAmountPrezzo;
+            if (result.ecoPoints.HasValue)
+            {
+                fillAmountPackaging = (float)result.ecoPoints.Value / (float)Massimo;
+            }
+            MaskPackagingFacile.fillAmount = fillAmountPackaging;
+            if (result.qualityPoints.HasValue)
+            {
+                fillAmountQuality = (float)result.qualityPoints.Value / (float)Massimo;
+            }
+            MaskQualityFacile.fillAmount = fillAmountQuality;
+        } else if (MenuPrincipale.levelDifficulty == 1)
+        {
+            fillAmountTotale = (float)result.totalPoints / (float)Massimo;
+            fillAmountPrezzo = (float)result.pricePoints / (float)Massimo;
+            MaskTotaleMedia.fillAmount = fillAmountTotale;
+            MaskPrezzoMedia.fillAmount = fillAmountPrezzo;
+            if (result.ecoPoints.HasValue)
+            {
+                fillAmountPackaging = (float)result.ecoPoints.Value / (float)Massimo;
+            }
+            MaskPackagingMedia.fillAmount = fillAmountPackaging;
+            if (result.qualityPoints.HasValue)
+            {
+                fillAmountQuality = (float)result.qualityPoints.Value / (float)Massimo;
+            }
+            MaskQualityMedia.fillAmount = fillAmountQuality;
+            if (result.originPoints.HasValue)
+            {
+                fillAmountProvenienza = (float)result.originPoints.Value / (float)Massimo;
+            }
+            MaskProvenienzaMedia.fillAmount = fillAmountProvenienza;
+        }
+        else
+        {
+            fillAmountTotale = (float)result.totalPoints / (float)Massimo;
+            fillAmountPrezzo = (float)result.pricePoints / (float)Massimo;
+            MaskTotaleDifficile.fillAmount = fillAmountTotale;
+            MaskPrezzoDifficile.fillAmount = fillAmountPrezzo;
+            if (result.ecoPoints.HasValue)
+            {
+                fillAmountPackaging = (float)result.ecoPoints.Value / (float)Massimo;
+            }
+            MaskPackagingDifficile.fillAmount = fillAmountPackaging;
+            if (result.qualityPoints.HasValue)
+            {
+                fillAmountQuality = (float)result.qualityPoints.Value / (float)Massimo;
+            }
+            MaskQualityDifficile.fillAmount = fillAmountQuality;
+            if (result.originPoints.HasValue)
+            {
+                fillAmountProvenienza = (float)result.originPoints.Value / (float)Massimo;
+            }
+            MaskProvenienzaDifficile.fillAmount = fillAmountProvenienza;
+            if (result.seasonPoints.HasValue)
+            {
+                fillAmountStagione = (float)result.seasonPoints.Value / (float)Massimo;
+            }
+            MaskStagioneDifficile.fillAmount = fillAmountStagione;
+        }       
+        
     }
 
     public void MainMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
+    }
+
+    public void ScelataSchermata()
+    {
+        if (MenuPrincipale.levelDifficulty == 0)
+        {
+            SchermataFacile.SetActive(true);       
+        } else if(MenuPrincipale.levelDifficulty == 1)
+        {
+            SchermataMedia.SetActive(true);
+        }
+        else
+        {
+            SchermataDifficile.SetActive(true);
+        }
     }
 }
