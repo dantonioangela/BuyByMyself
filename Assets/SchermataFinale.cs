30576c40abbfc04a642b1a50ce30277dd528c449
GiacomoOffre <giacomo.offre@gmail.com>
2022-02-16 15:32:34 +0100

    Percentuali provvisorie nei punteggi

--- Assets/SchermataFinale.cs
+++ Assets/SchermataFinale.cs
@@ -7,8 +7,18 @@ using UnityEngine.SceneManagement;
 public class SchermataFinale : MonoBehaviour
 {
     public float Massimo;
-    public float ValoreCorrente;
-    public Image Mask;
+    public float TotaleGiocatore=50;
+    public float PackagingGiocatore = 30;
+    public float QualityGiocatore = 70;
+    public float StagioneGiocatore = 10;
+    public float ProvenienzaGiocatore = 90;
+    public float PrezzoGiocatore = 20;
+    public Image MaskTotale;
+    public Image MaskPackaging;
+    public Image MaskQuality;
+    public Image MaskStagione;
+    public Image MaskProvenienza;
+    public Image MaskPrezzo;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +28,27 @@ public class SchermataFinale : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmountTotale = (float)TotaleGiocatore / (float)Massimo;
+        MaskTotale.fillAmount = fillAmountTotale;
+
+        float fillAmountPackaging = (float)PackagingGiocatore / (float)Massimo;
+        MaskPackaging.fillAmount = fillAmountPackaging;
+
+        float fillAmountQuality = (float)QualityGiocatore / (float)Massimo;
+        MaskQuality.fillAmount = fillAmountQuality;
+
+        float fillAmountStagione = (float)StagioneGiocatore / (float)Massimo;
+        MaskStagione.fillAmount = fillAmountStagione;
+
+        float fillAmountProvenienza = (float)ProvenienzaGiocatore / (float)Massimo;
+        MaskProvenienza.fillAmount = fillAmountProvenienza;
+        float fillAmountPrezzo = (float)PrezzoGiocatore / (float)Massimo;
+        MaskPrezzo.fillAmount = fillAmountPrezzo;
     }
 
     public void MainMenu()
